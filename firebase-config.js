23bacad44bfa9b9de8ec5f0c0dbf725832792619
payalbhosale331 <payalbhosale331@gmail.com>
2026-08-11 17:03:13 +0530

    Fixed index links

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-database.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-database.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB48-aB-CrurCoDgm-5jm2MIYXzqXWB1XQ",
@@ -17,6 +18,7 @@ const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 const database = getDatabase(app);
+const storage = getStorage(app);
 
-export { app, auth, database };
+export { app, auth, database, storage };
 
